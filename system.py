20d11a84db989b55dc363df42d46a6819db84562
Hryts <hrytsay@ucu.edu.ua>
2020-03-10 00:19:03 +0200

    works now

--- system.py
+++ system.py
@@ -20,7 +20,7 @@ class System:
             res = []
             for coord in range(len(coordinates)):
                 state = coordinates[coord]
-                if state + 1 < len(coordinates):
+                if state + 1 < self.el_in_dim:
                     res.append(coordinates[:coord] + [state + 1] + coordinates[coord+1:])
                 if state - 1 >= 0:
                     res.append(coordinates[:coord] + [state - 1] + coordinates[coord+1:])
@@ -37,9 +37,8 @@ class System:
         modified = False
 
         for i in range(self.n):
-            increment_coord(len(current_coord) - 1)
+            # print(current_coord)
             neighbours = [System.get(self.system, c) for c in get_neighbours(current_coord)]
-
             delta_u = 0
             current_state = System.get(self.system, current_coord)
 
@@ -52,10 +51,15 @@ class System:
                 System.set(self.system, current_coord, current_state)
                 modified = True
 
+            increment_coord(len(current_coord) - 1)
+
         yield modified
 
     @staticmethod
     def get(system, coordinates):
+        print(f'system: {system}')
+        print(f'coords: {coordinates}')
+        print(f'element: {system[coordinates[0]]}\n')
         if len(coordinates) == 1:
             return system[coordinates[0]]
         return System.get(system[coordinates[0]], coordinates[1:])
@@ -64,7 +68,8 @@ class System:
     def set(system, coordinates, value):
         if len(coordinates) == 1:
             system[coordinates[0]] = value
-        System.set(system[coordinates[0]], coordinates[1:], value)
+        else:
+            System.set(system[coordinates[0]], coordinates[1:], value)
 
     @staticmethod
     def pdf():
